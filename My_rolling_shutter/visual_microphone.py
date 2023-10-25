68108b755d5d786978d2fe05c0c007ef687b0006
yuuki330 <katsuyu107@gmail.com>
2023-10-26 08:13:15 +0900

    下駄履かせ実装

--- My_rolling_shutter/visual_microphone.py
+++ My_rolling_shutter/visual_microphone.py
@@ -12,10 +12,9 @@ from time import time
 from scipy.io.wavfile import write
 import pandas as pd
 from statsmodels.tsa.ar_model import AutoReg
-from statsmodels.tsa.ar_model import ar_select_order
-import librosa
+# import librosa
 import resampy
-import scipy.stats
+# import scipy.stats
 
 class VM_rolling:
     ## コンストラクタの定義
@@ -37,14 +36,12 @@ class VM_rolling:
         tshift = np.argmax(signal.fftconvolve(x, np.flip(y))) + 1
         ## シフト幅を計算
         shift = y.size - tshift
-        # print(f'shift: {shift}')
         ## 元信号をシフトして返す
         return np.roll(x,shift)
     
-    def plot_spectrogram(self, x, fs=2200):
+    def plot_spectrogram(self, x, fs=2000):
         # 入力波形をリサンプリング
         y = resampy.resample(x, sr_orig=61920, sr_new=2000)
-        print(f'max')
         plt.figure()
         plt.specgram(y, Fs=fs, cmap=plt.get_cmap('jet'))
         plt.xlabel('Time (sec)')
@@ -53,37 +50,6 @@ class VM_rolling:
         plt.colorbar().set_label('PSD(dB)')
         plt.savefig('./result/spectrogram')
         plt.close()
-        
-        del_fs = 1032
-        fft_y = fft(x)
-        # plt.figure()
-        # plt.plot(fft_y)
-        # plt.xlabel('Frequency (Hz)')
-        # plt.xlim(0, 12000)
-        # plt.savefig('./result/fft_x')
-        # plt.close()
-
-        # print(np.where(fft_y>0.5))
-        # print(np.where(fft_y<-0.5))
-        frequency_index = [i for i in range(0, 40000, del_fs)]
-        # fft_y[np.where(fft_y>0.1)] = 0
-        # fft_y[np.where(fft_y<-0.1)] = 0
-        # plt.figure()
-        # plt.plot(fft_y)
-        # plt.xlabel('Frequency (Hz)')
-        # plt.xlim(0, 12000)
-        # plt.savefig('./result/fft_x_del')
-        # plt.close()
-
-        # y = ifft(fft_y)
-        # plt.figure()
-        # plt.specgram(y, Fs=fs, cmap=plt.get_cmap('jet'))
-        # plt.xlabel('Time (sec)')
-        # plt.ylabel('Frequency (Hz)')
-        # plt.ylim(0, 1100)
-        # plt.colorbar().set_label('PSD(dB)')
-        # plt.savefig('./result/spectrogram_del')
-        # plt.close()
 
     def get_scaled_sound(self, sound):
         maxs = np.max(sound)
@@ -96,10 +62,11 @@ class VM_rolling:
             sound -= offset
         return sound
     
-    def save_audio(self, file_name, sound, sr):
+    def save_audio(self, file_name, x, sr):
+        y = resampy.resample(x, sr_orig=61920, sr_new=2000)
         if sr==0:
             raise Exception("Invalid sampling rate")
-        write(file_name, sr, sound)
+        write(file_name, sr, y)
     
     def sound_from_video(self, nscale, norientation, downsample_factor):
         ## Parameters
@@ -111,7 +78,7 @@ class VM_rolling:
         Num_signal = (self.height+N_gap) * self.nframes
 
         ## 結果用のパラメータ
-        stop_iter = 100
+        stop_iter = 500
         start_idx = 0
         end_idx = (stop_iter-1)*(self.height+N_gap) # -1するのは初期フレームと差分を取るところを除外するため
 
@@ -207,64 +174,67 @@ class VM_rolling:
         for band in recovered_signal:
             plt.figure(figsize=[20, 5])
             plt.plot(range(start_idx, end_idx), recovered_signal[band][start_idx:end_idx])
-            # plt.plot(range(len(recovered_signal[band])), recovered_signal[band])
-            plt.savefig('./result/補完前_'+str(band))
+            plt.savefig('./result/補間前_'+str(band))
             plt.close()
         
-        for j in range(10):
+        jmax = 10
+        for j in range(jmax):
             ## 繰り返し一括補完
             for band in recovered_signal:
                 recovered_signal[band] = recovered_signal[band][start_idx:end_idx]
                 forward_pred = []
-                ar_model = AutoReg(recovered_signal[band], lags=720)
+                ar_model = AutoReg(recovered_signal[band], lags=150)
                 ar_model = ar_model.fit()
                 for i in range(0, len(recovered_signal[band]), self.height+N_gap):
                     if stop_iter == i//(self.height+N_gap):
                         break
                     forward_pred.append(ar_model.predict(start=(self.height+i)+1, end=i+self.height+N_gap))
                     recovered_signal[band][(i+self.height):i+self.height+N_gap] = forward_pred[(i//(self.height+N_gap))]
+
+                    ## predの最終値と次のフレームの最初の値を揃えるために、以降の値にpredの最終値と次のフレームの差分を足す
+                    if len(recovered_signal[band]) > i+self.height+N_gap+1:
+                        recovered_signal[band][i+self.height+N_gap:] += recovered_signal[band][i+self.height+N_gap+1] - recovered_signal[band][i+self.height+N_gap]
+
                     if(i%100==0):
                         time_present = time()
-                        print(f'{band}_進捗: {i}/{len(recovered_signal[band])}, 経過時間: {time_present-time_start:.2f}秒')
+                        print(f'{j}_{band}_進捗: {i}/{len(recovered_signal[band])}, 経過時間: {time_present-time_start:.2f}秒')
 
             ## グラフを保存
             for band in recovered_signal:
                 plt.figure(figsize=[20, 5])
                 plt.plot(range(start_idx, end_idx), recovered_signal[band][start_idx:end_idx])
                 # plt.plot(range(len(recovered_signal[band])), recovered_signal[band])
-                plt.savefig(f'./result/補完後_{j}'+str(band))
+                plt.savefig(f'./result/補間後_{j}'+str(band))
                 plt.close()
-        print('補完処理完了')
+        print('補間処理完了')
     
-        
-        j = 0
         ## 復元音声変数の初期化
         recov_sound = np.zeros(len(recovered_signal[(0,0)]))
+        j = 0
         for band in recovered_signal:
-            print(band)
-            # if j == 3:
-                # print('aaa')
-            recov_sound += self.align(np.array(recovered_signal[band]), np.array(recov_sound))
+            if j == 3:
+                recov_sound += self.align(np.array(recovered_signal[band]), np.array(recov_sound))
+            j += 1
             ## グラフを保存
             # plt.figure(figsize=[20, 5])
             # plt.plot(range(start_idx, end_idx), self.align(np.array(recovered_signal[band]), np.array(recov_sound))[start_idx:end_idx])
             # # plt.plot(range(len(recov_sound)), recov_sound)
             # plt.savefig('./result/align後_'+str(band))
             # plt.close()
-            j += 1
             plt.figure(figsize=[20, 5])
             plt.plot(range(start_idx, end_idx), recov_sound[start_idx:end_idx])
             # plt.plot(range(len(recov_sound)), recov_sound)
             plt.savefig('./result/合成後_'+str(band))
             plt.close()
-        print(f'max:{recov_sound.max()}')
-        print(f'min:{recov_sound.min()}')
+
         ## 復元音声のスペクトログラムを保存
         self.plot_spectrogram(recov_sound)
+    
+        return recov_sound
 
 
 video_path = './data/KitKat-60Hz-RollingShutter-Mary_MIDI-input.avi'
 video = VM_rolling(video_path)
 # x = video.sound_from_video(1, 1, 1)
 x = video.sound_from_video(5, 1, 1)
-# video.save_audio('test.wav', x, 2200)
+video.save_audio('test.wav', x, 2000)
